b4b60544db3f8bebae112a6b065437fc16c188b9
mzwiessele <ibinbei@gmail.com>
2016-03-17 11:17:35 +0000

    [load data] bug fixes

--- py_notebooks/collect_and_name_data.py
+++ py_notebooks/collect_and_name_data.py
@@ -2,15 +2,28 @@ import sys
 
 execfile('load_data.py')
 
-all_data = [
-    rnaseq_all_genes,#: (11216, 104)
-    rnaseq_effector_genes,#: (1044, 104)
-    rnaseq_mod_activities,#: (89, 104)
-    rnaseq_mod_genevalues,#: (172, 104)
-    rnaseq_mod_nodevalues,#: (462, 104)
-    rnaseq_signaling_genes,#: (2184, 104)
-#    rnaseq_design: (104, 4)
-]
+if sys.argv[1] in 'rnaseq':
+    all_data = [
+        rnaseq_all_genes,#: (11216, 104)
+        rnaseq_effector_genes,#: (1044, 104)
+        rnaseq_mod_activities,#: (89, 104)
+        rnaseq_mod_genevalues,#: (172, 104)
+        rnaseq_mod_nodevalues,#: (462, 104)
+        rnaseq_signaling_genes,#: (2184, 104)
+    #    rnaseq_design: (104, 4)
+    ]
+    design = rnaseq_design
+else:
+    all_data = [
+        micro_all_genes,#: (13613, 105)
+        micro_effector_genes,#: (1044, 105)
+        micro_mod_activities,#: (89, 105)
+        micro_mod_genevalues,#: (201, 105)
+        micro_mod_nodevalues,#: (462, 105)
+        micro_signaling_genes,#: (2184, 105)
+        micro_design,#: (105, 4)
+    ]
+    design = micro_design
 
 names = ['all_genes', 
          'effector_genes', 
@@ -22,5 +35,3 @@ names = ['all_genes',
 
 for y,n in zip(all_data, names):
     y.name = n
-
-design = rnaseq_design
